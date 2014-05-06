55d9bad87bbb14065fbdf2b2071d8c61f86de007
Misty Parshall <mparsha1@ycp.edu>
2014-05-05 21:28:53 -0400

    Added Upcoming Events to HomePage

--- WeddingSiteGWT/src/weddingsite/client/HomePageView.java
+++ WeddingSiteGWT/src/weddingsite/client/HomePageView.java
@@ -1,18 +1,21 @@
 package weddingsite.client;
 
-import weddingsite.client.Site.Pages;
+import weddingsite.shared.Activity;
+import weddingsite.shared.EventsModel;
+import weddingsite.shared.GetItemsResult;
 import weddingsite.shared.IPublisher;
 import weddingsite.shared.ISubscriber;
-import weddingsite.shared.LoginResult;
 
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
-import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.ui.LayoutPanel;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.ui.MenuItemSeparator;
+import com.google.gwt.user.client.ui.ScrollPanel;
 
 public class HomePageView  extends Composite implements ISubscriber {
 	private MenuItem eventsItem;
@@ -22,19 +25,96 @@ public class HomePageView  extends Composite implements ISubscriber {
 	private MenuItem calendarItem;
 	private MenuItem mntmAccount;
 	private MenuItem mntmSeatingCharts;
+	private FlowPanel mainFlowPanel;
+	private ScrollPanel mainScrollPanel;
+	private Label TitleLbl;
+	private Label FirstEventName;
+	private Label FirstEventDate;
+	private Label SecondEventName;
+	private Label SecondEventDate;
 	
 	public HomePageView() {
 		
+		mainFlowPanel = new FlowPanel();
+		initWidget(mainFlowPanel);
+		mainFlowPanel.setSize("2000px", "2000px");
+		
+		mainScrollPanel = new ScrollPanel();
+		mainFlowPanel.add(mainScrollPanel);
+		mainScrollPanel.setSize("100%", "100%");
+		
+		
+		
 		layoutPanel = new LayoutPanel();
-		layoutPanel.setStyleName("layout");
-		initWidget(layoutPanel);
-		layoutPanel.setSize("100%", "100%");
+		layoutPanel.setStyleName("InnerBackgroundCenter");
+		mainScrollPanel.add(layoutPanel);
+		layoutPanel.setSize("900px", "1000px");
 		
 		menuBar = new MenuBar(true);
 		layoutPanel.add(menuBar);
-		layoutPanel.setWidgetLeftWidth(menuBar, 5, Unit.PCT, 40, Unit.PCT);
-		layoutPanel.setWidgetTopHeight(menuBar, 5, Unit.PCT, 80, Unit.PCT);
+		layoutPanel.setWidgetLeftWidth(menuBar, 5, Unit.PCT, 23.1, Unit.PCT);
+		layoutPanel.setWidgetTopHeight(menuBar, 5, Unit.PCT, 30.0, Unit.PCT);
 		menuBar.setStyleName("menuBar .gwt-MenuBar");
+		
+		EventsModel model = new EventsModel();
+		model.setAccountName(Site.currentUser.getAccountName());
+		
+		RPC.getEventsService.GetEvents(model, new AsyncCallback<GetItemsResult<Activity>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onSuccess(GetItemsResult<Activity> result) {
+				Activity first = new Activity();
+				Activity second = new Activity();
+				
+				int firstYear = 3000;
+				int firstMonth = 12;
+				int firstDay = 31;
+				int secondYear = 3000;
+				int secondMonth = 12;
+				int secondDay = 31;
+				
+				if(result.getResult().size() > 0) {
+				
+					for(Activity a : result.getResult()) {
+						String[] tokens = a.getDate().split("[/]");
+						int month = Integer.parseInt(tokens[0]);
+						int day = Integer.parseInt(tokens[0]);
+						int year = Integer.parseInt(tokens[0]);
+						
+						if(year < firstYear) {
+							first = a;
+						} else if(year <= firstYear && month < firstMonth) {
+							first = a;
+						} else if(year <= firstYear && month <= firstMonth && day < firstDay) {
+							first = a;
+						} else if(year <= firstYear && month <= firstMonth && day < firstDay) {
+							second = a;
+						} else if(year < secondYear) {
+							second = a;
+						} else if(year <= secondYear && month < secondMonth) {
+							second = a;
+						} else if(year <= secondYear && month <= secondMonth && day < secondDay) {
+							second = a;
+						}
+						
+					}
+					
+					FirstEventName.setText(first.getTitle());
+					FirstEventDate.setText(first.getDate());
+					SecondEventName.setText(second.getTitle());
+					SecondEventDate.setText(second.getDate());;
+				} else {
+					FirstEventName.setText("There are no upcoming events");
+				}
+			}
+			
+		});
 		
 		AttendanceLists = new MenuItem("Attendance Lists", false, new Command() {
 			public void execute() {
@@ -79,6 +159,36 @@ public class HomePageView  extends Composite implements ISubscriber {
 		});
 		
 		menuBar.addItem(mntmSeatingCharts);
+		
+		FlowPanel flowPanel = new FlowPanel();
+		layoutPanel.add(flowPanel);
+		layoutPanel.setWidgetLeftWidth(flowPanel, 391.0, Unit.PX, 404.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(flowPanel, 68.0, Unit.PX, 697.0, Unit.PX);
+		
+		TitleLbl = new Label("Upcoming Events");
+		flowPanel.add(TitleLbl);
+		TitleLbl.setHeight("32px");
+		TitleLbl.setStyleName("CenterTitles");
+		
+		FirstEventName = new Label("");
+		FirstEventName.setStyleName("CenterText");
+		flowPanel.add(FirstEventName);
+		FirstEventName.setSize("100%", "40px");
+		
+		FirstEventDate = new Label("");
+		FirstEventDate.setStyleName("CenterText");
+		flowPanel.add(FirstEventDate);
+		FirstEventDate.setSize("100%", "40px");
+		
+		SecondEventName = new Label("");
+		SecondEventName.setStyleName("CenterText");
+		flowPanel.add(SecondEventName);
+		SecondEventName.setSize("100%", "40px");
+		
+		SecondEventDate = new Label("");
+		SecondEventDate.setStyleName("CenterText");
+		flowPanel.add(SecondEventDate);
+		SecondEventDate.setSize("100%", "40px");
 	}
 	
 	
